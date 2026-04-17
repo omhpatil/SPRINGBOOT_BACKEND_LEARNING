e479f3be19156f60886b4011223f1d62768e00eb
Om Patil <patilom887@gmail.com>
2026-04-17 12:06:05 +0530

    CI-CD changes

--- A06_ServiceLayer/src/main/java/com/main/controller/EmployeeController.java
+++ A06_ServiceLayer/src/main/java/com/main/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     // Here i have made change for checking jenkins pipeline is working correct or not
